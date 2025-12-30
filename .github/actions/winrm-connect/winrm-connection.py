33238468b8f9bca719e3e9f2504c4c2f317e7c46
Suryanshu Sharma <suryanshu.sh@outlook.com>
2025-12-31 01:36:56 +0530

    created composite action instead of resuable workflow

---
+++ .github/actions/winrm-connect/winrm-connection.py
@@ -0,0 +1,18 @@
+import os
+import sys
+
+import winrm
+
+server_name = sys.argv[1]
+username = os.environ["WIN_UN"]
+password = os.environ["WIN_PW"]
+
+session = winrm.Session(f"http://${server_name}:5985/wsman", auth=(username, password))
+
+result = session.run("hostname")
+
+print("STDOUT:", result.std_out.decode("utf-8"))
+print("STDERR:", result.std_err.decode("utf-8"))
+
+if result.status_code != 0:
+    raise Exception(f"Command failed with status code {result.status_code}")
