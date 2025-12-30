18d0c0cb3c586fb568fa008e774ace89a651dd8b
Suryanshu Sharma <suryanshu.sh@outlook.com>
2025-12-31 02:18:02 +0530

    added custom powershell commands

--- .github/actions/winrm-connect/winrm-connection.py
+++ .github/actions/winrm-connect/winrm-connection.py
@@ -4,6 +4,8 @@ import sys
 import winrm
 
 server_name = sys.argv[1]
+pwsh_command = sys.argv[2]
+
 username = os.environ["WIN_UN"]
 password = os.environ["WIN_PW"]
 print(f"http://{server_name}:5985/wsman")
@@ -12,7 +14,7 @@ session = winrm.Session(
     f"http://{server_name}:5985/wsman", auth=(username, password), transport="ntlm"
 )
 
-result = session.run_ps("hostname")
+result = session.run_ps(pwsh_command)
 
 print("STDOUT:", result.std_out.decode("utf-8"))
 print("STDERR:", result.std_err.decode("utf-8"))
